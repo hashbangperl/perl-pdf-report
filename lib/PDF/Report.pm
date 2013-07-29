e01b94324b03b889bd664653ee212a7ae9282208
matteplus <teo.ebbasta@gmail.com>
2013-07-29 16:16:29 +0100

    add optional 'align' parameter to addParagraph
    Without this change it's very hard to center-align a paragraph that spans multiple lines. The module philosophy is to account for alignment in hPos (calculated before calling the method). Anyway this leads to troubles when trying to print a center-aligned paragraph that spans multiple lines: the first line starts at the right position, the following lines start aligned on the left

--- lib/PDF/Report.pm
+++ lib/PDF/Report.pm
@@ -533,8 +533,9 @@ $indent is the number of spaces at the beginning of the first line.
 =cut
 
 sub addParagraph {
-  my ( $self, $text, $hPos, $vPos, $width, $height, $indent, $lead ) = @_;
+  my ( $self, $text, $hPos, $vPos, $width, $height, $indent, $lead, $align ) = @_;
 
+  $align ||= 'justified';
   my $txt = $self->{page}->text;
   $txt->font($self->{font}, $self->{size});
 
@@ -544,7 +545,7 @@ sub addParagraph {
 #  0.40.x
   $txt->lead($lead); # Line spacing
   $txt->translate($hPos,$vPos);
-  $txt->paragraph($text, $width, $height, -align=>'justified');
+  $txt->paragraph($text, $width, $height, -align => $align);
 
   ($self->{hPos},$self->{vPos}) = $txt->textpos;
 }
